4c13641503fda9b3dc6f31ac902237a4aadc73d5
Naooomi <naomi.wuuu@gmail.com>
2017-04-27 22:20:31 +1200

    First App

---
+++ Learning/MyFirstApp/MyFirstApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstApp
+//
+//  Created by Naomi Wu on 27/04/17.
+//  Copyright © 2017 Naomi Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Learning/MyFirstApp/MyFirstApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstApp
+//
+//  Created by Naomi Wu on 27/04/17.
+//  Copyright © 2017 Naomi Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
