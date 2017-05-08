cd3acfb1fc78bfc31e07a2e490b66c9b9aac6966
Naooomi <naomi.wuuu@gmail.com>
2017-05-08 23:28:36 +1200

    LondonLandmarks first accomplishment

---
+++ Practice/LondonLandmarks/LondonLandmarks/TableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.m
+//  LondonLandmarks
+//
+//  Created by Naomi Wu on 2/05/17.
+//  Copyright © 2017 Naomi Wu. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
