ce5b2b37524a55f98c1ddf25b5874d5e53c2a7c6
Naooomi <naomi.wuuu@gmail.com>
2017-05-22 22:59:17 +1200

    Obj Variable Leaarning

---
+++ Learning/ObjVariables/ObjVariables/ViewController.h
@@ -0,0 +1,23 @@
+//
+//  ViewController.h
+//  ObjVariables
+//
+//  Created by Naomi Wu on 22/05/17.
+//  Copyright © 2017 Naomi Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *label1;
+@property (strong, nonatomic) IBOutlet UILabel *label2;
+@property (strong, nonatomic) IBOutlet UILabel *label3;
+
+@property (strong, nonatomic) IBOutlet UIButton *button;
+
+@property (strong, nonatomic) IBOutlet UISwitch *switches;
+
+
+@end
+
