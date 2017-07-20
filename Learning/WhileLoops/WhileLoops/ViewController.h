d72f0db476b8b7b4585a3bcd4772e54ad54d0747
Naooomi <naomi.wuuu@gmail.com>
2017-07-20 22:22:39 +1200

    While Loops

---
+++ Learning/WhileLoops/WhileLoops/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhileLoops
+//
+//  Created by Naomi Wu on 20/07/17.
+//  Copyright © 2017 Naomi Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Learning/WhileLoops/WhileLoops/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WhileLoops
+//
+//  Created by Naomi Wu on 20/07/17.
+//  Copyright © 2017 Naomi Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
