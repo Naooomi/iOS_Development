4dbc4d37f3d7c3d282294b4e027c58a1cf7b9bbf
Naooomi <naomi.wuuu@gmail.com>
2017-08-03 20:47:32 +1200

    Shake Me Silly Game finish

---
+++ Learning/ShakeMeSilly/ShakeMeSilly/ViewController.h
@@ -0,0 +1,36 @@
+//
+//  ViewController.h
+//  ShakeMeSilly
+//
+//  Created by Naomi Wu on 3/08/17.
+//  Copyright © 2017 Naomi Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController {
+    
+    NSTimer *timer;
+    
+    int timeInt;
+    int scoreInt;
+    
+    int modeInt;
+    int imageInt;
+    
+    
+}
+
+@property (weak, nonatomic) IBOutlet UILabel *timerLabel;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIButton *startGameButton;
+
+
+- (IBAction)startGame:(id)sender;
+
+
+
+
+@end
+
