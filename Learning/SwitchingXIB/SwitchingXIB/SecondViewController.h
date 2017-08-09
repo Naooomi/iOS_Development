28611bdf9643f5dab83f175a5282362fe7e160d2
Naooomi <naomi.wuuu@gmail.com>
2017-08-09 21:15:52 +1200

    Switching to XIB finish

---
+++ Learning/SwitchingXIB/SwitchingXIB/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  SwitchingXIB
+//
+//  Created by Naomi Wu on 9/08/17.
+//  Copyright © 2017 Naomi Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+- (IBAction)dismissView:(id)sender;
+
+@end
---
+++ Learning/SwitchingXIB/SwitchingXIB/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  SwitchingXIB
+//
+//  Created by Naomi Wu on 9/08/17.
+//  Copyright © 2017 Naomi Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)switchXIB:(id)sender;
+
+@end
+
