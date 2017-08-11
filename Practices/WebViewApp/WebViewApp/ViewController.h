fdf5f456105e2cbab81e6babc8d616ea63c1fd22
Naooomi <naomi.wuuu@gmail.com>
2017-08-11 13:31:51 +1200

    Web View App to show Apple website

---
+++ Practices/WebViewApp/WebViewApp/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  WebViewApp
+//
+//  Created by Naomi Wu on 11/08/17.
+//  Copyright © 2017 Naomi Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@end
+
---
+++ Practices/WebViewApp/WebViewApp/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  WebViewApp
+//
+//  Created by Naomi Wu on 11/08/17.
+//  Copyright © 2017 Naomi Wu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.apple.com"]]];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
